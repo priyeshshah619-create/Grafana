603b7d441c17a0b2ac6be84cc9bda74144043a0a
priyeshshah619-create <priyesh.shah619@gmail.com>
2026-05-23 16:53:07 +0530

    Update plugin_lambda.py

--- cfn/plugin_lambda.py
+++ cfn/plugin_lambda.py
@@ -1,19 +1,30 @@
 import boto3
 import cfnresponse
+import json
 
 def handler(event, context):
+    # Condition A & B: Custom Resource to install plugins
     if event['RequestType'] == 'Delete':
         cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
         return
     
     try:
+        workspace_id = event['ResourceProperties']['WorkspaceId']
         grafana = boto3.client('grafana')
-        # Condition A: Install Plugins
-        config = '{"plugins": [{"pluginId": "grafana-clock-panel", "pluginVersion": "1.0.0"}]}'
+        
+        # Configuration payload for plugins
+        config = {
+            "plugins": [
+                {"pluginId": "grafana-clock-panel", "pluginVersion": "1.0.0"}
+            ]
+        }
+        
         grafana.update_workspace_configuration(
-            workspaceId=event['ResourceProperties']['WorkspaceId'],
-            configuration=config
+            workspaceId=workspace_id,
+            configuration=json.dumps(config)
         )
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
+        
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, {'Status': 'Plugins Installed'})
     except Exception as e:
+        print(f"Error: {str(e)}")
         cfnresponse.send(event, context, cfnresponse.FAILED, {'Error': str(e)})
