408440c433e4993b127f4a4b8544c91a145e7a29
priyeshshah619-create <priyesh.shah619@gmail.com>
2026-05-25 16:34:14 +0530

    Update lambda.py

--- cfn/lambda.py
+++ cfn/lambda.py
@@ -1,1 +1,14 @@
+import boto3
+import cfnresponse
 
+def handler(event, context):
+    grafana = boto3.client('grafana')
+    try:
+        if event['RequestType'] in ['Create', 'Update']:
+            ws = event['ResourceProperties']['WS']
+            plugins = event['ResourceProperties']['Plugins']
+            for plugin in plugins:
+                grafana.install_plugin(workspaceId=ws, pluginId=plugin)
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
+    except Exception as e:
+        cfnresponse.send(event, context, cfnresponse.FAILED, {"Error": str(e)})
