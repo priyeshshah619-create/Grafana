d9efdc32943b26fdb2d4b0cde09bda45b9797727
priyeshshah619-create <priyesh.shah619@gmail.com>
2026-05-30 00:46:23 +0530

    Update plugin_updater.py

--- lambda/plugin_updater.py
+++ lambda/plugin_updater.py
@@ -1,26 +1,20 @@
-import boto3
-import json
-import urllib3
-import traceback
+import boto3, json, urllib3
 
 http = urllib3.PoolManager()
 
-def cfn_send(event, context, response_status, response_data, reason=None):
+def cfn_send(event, context, status, data, reason=None):
     response_body = {
-        'Status': response_status,
-        'Reason': reason or "See the details in CloudWatch Log Stream: " + context.log_stream_name,
-        'PhysicalResourceId': context.log_stream_name,
+        'Status': status,
+        'Reason': reason or "Success",
+        'PhysicalResourceId': event.get('PhysicalResourceId', 'PluginUpdater'),
         'StackId': event['StackId'],
         'RequestId': event['RequestId'],
         'LogicalResourceId': event['LogicalResourceId'],
-        'Data': response_data
+        'Data': data
     }
-    json_response_body = json.dumps(response_body)
-    headers = {'content-type': '', 'content-length': str(len(json_response_body))}
-    try:
-        http.request('PUT', event['ResponseURL'], body=json_response_body, headers=headers)
-    except Exception as e:
-        print(f"Failed to send cfnresponse: {str(e)}")
+    json_body = json.dumps(response_body)
+    headers = {'content-type': '', 'content-length': str(len(json_body))}
+    http.request('PUT', event['ResponseURL'], body=json_body, headers=headers)
 
 def lambda_handler(event, context):
     try:
@@ -28,13 +22,10 @@ def lambda_handler(event, context):
             cfn_send(event, context, 'SUCCESS', {})
             return
         
-        workspace_id = event['ResourceProperties']['WorkspaceId']
         client = boto3.client('grafana')
-        config = {"plugins": {"pluginAdminEnabled": True, "pluginIds": ["grafana-piechart-panel", "grafana-clock-panel"] } }
-        
-        client.update_workspace_configuration(workspaceId=workspace_id, configuration=json.dumps(config))
+        config = {"plugins": {"pluginAdminEnabled": True, "pluginIds": ["grafana-piechart-panel", "grafana-clock-panel"]}}
+        client.update_workspace_configuration(workspaceId=event['ResourceProperties']['WorkspaceId'], configuration=json.dumps(config))
         
         cfn_send(event, context, 'SUCCESS', {"Status": "Finished"})
     except Exception as e:
-        print(f"Error: {str(e)}")
         cfn_send(event, context, 'FAILED', {"Error": str(e)}, reason=str(e))
